6f6e215b8ada17d0396a1d5d01ce843c85497334
SunflowerKun <dream26322gmail.com>
2022-02-25 12:52:52 +0600

    Initial commit

---
+++ src/com/company/Car.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Car implements AutoCloseable {
+    @Override
+    public void close() throws Exception {
+        System.out.println("Машина жабылып жатат!!!");
+    }
+
+    public void drive() {
+        System.out.println("Машина журуп жатат");
+    }
+
+}
