f525fc283cda27e59c5d141433d4ea98c605ba95
SunflowerKun <dream26322gmail.com>
2022-02-26 19:05:22 +0600

    try with resourses

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,15 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) throws RuntimeException {
-        try {
-            Car car = new Car();
+        try(Car car = new Car()) {
             car.drive();
             car.setName("Mersedes");
-            car.setYear(2020);
+            car.setYear(15);
             System.out.println("Машинанын аты: "+car.getName()+", "+car.getName()+" жылы: "+car.getYear());
-            car.close();
         } catch (InvalidNameException | InvalidYearException  n) {
             System.err.println(n.getMessage());
+
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
